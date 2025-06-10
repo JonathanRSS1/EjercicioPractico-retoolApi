4d7ce1cde9d57dc8114bd61ae2671ae7f7221f61
JonathanRSS1 <139492269+JonathanRSS1@users.noreply.github.com>
2025-06-10 17:34:46 -0600

    Delete Evaluacion-15 directory

--- Evaluacion-15/backend/src/routes/registerEmployees.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import registerEmployeeController from "../controllers/registerEmployeeController.js";
-const router = express.Router();
-
-router.route("/").post(registerEmployeeController.register)
-
-export default router
